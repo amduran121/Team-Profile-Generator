b61992f0481237e646f0e9a03cb57d0ac90fb275
Austin Duran <amduran121@gmail.com>
2021-12-01 16:26:27 -0700

    push

--- index.js
+++ index.js
@@ -13,43 +13,116 @@ const idArray = [];
 function app() {
   function createManager() {
     console.log("Enter information to build your team");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "Input manager's name",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "You must enter at least one character";
+          },
+        },
+        {
+          name: "managerId",
+          message: "Input manager's ID number",
+        },
+        {
+          name: "managerEmail",
+          message: "Input manager's email address",
+        },
+        {
+          name: "managerOfficeNumber",
+          message: "Input manager's Office Number",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+          }
+  function Intern() {
+    console.log("Enter information for your interns");
     inquirer.prompt([
       {
         type: "input",
-        name: "managerName",
-        message: "Input manager's name",
-        validate: (answer) => {
-          if (answer !== "") {
-            return true;
-          }
-          return "You must enter at least one character"
-        },
+        name: "internName",
+        message: "Input intern(s) name(s)",
       },
       {
-          name: "managerId",
-          message: "Input manager's ID number"
-        
+        name: "internId",
+        message: "Input interns name",
       },
       {
-          name: "managerEmail",
-          message: "Input manager's email address"
+        name: "internEmail",
+        message: "Input interns email",
       },
-      {
-          name: "managerOfficeNumber",
-          message: "Input manager's Office Number"
-      }
-    ])
-    .then(answers => {
-        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
-        teamMembers.push(manager)
-        idArray.push(answers.managerId)
-        createTeam()
-        
-    })
+       .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+    ]);
+    
   }
+   function Employee() {
+     console.log("Enter information for your interns");
+     inquirer.prompt([
+       {
+         type: "input",
+         name: "employee name",
+         message: "Input employee(s) name(s)",
+       },
+       {
+         name: "employeeId",
+         message: "Input interns ID number",
+       },
+       {
+         name: "employeeEmail",
+         message: "Input employees email",
+       },
+     ]);
+   }
+
   function createTeam() {
-    //inquirer prompt what ktype of team member to add, after that we need a .then to assess the user choice, and inside of that .then we will write a switch case, and depening on response we will add engineer or intern or create the site.      
-}
+    //inquirer prompt what type of team member to add, after that we need a .then to assess the user choice, and inside of that .then we will write a switch case, and depending on response we will add engineer or intern or create the site.
+    var inquirer = require("inquirer");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "numEmployees",
+        message: "Enter number of employees",
+      },
+      {
+        name: "numEngineers",
+        message: "Enter number of engineers",
+      },
+      {
+        name: "numInterns",
+        message: "Enter number of interns",
+      },
+
+      // .then(answers => {
+      //   const createTeam = new
+      // })
+    ]);
+  }
   createManager();
 }
 
